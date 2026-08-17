9d37de6a05667813947faf7ee7704caf4fddfe2a
Andres Tapia <TU_CORREO@gmail.comandrestapia@cne.gob.ec>
2026-08-17 16:41:49 -0500

    Resto 1 completado

---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+// page = fixture
+
+test('login to hrm', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(
+    page.getByRole('link', { name: 'Admin' })
+  ).toBeVisible();
+});
